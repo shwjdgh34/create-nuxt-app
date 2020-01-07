83ef9946d1d29035179848b265b670b07dac7212
Jeongho Noh <nossi8128@gmail.com>
2020-01-07 17:42:57 +0900

    add users/_id page to get id parameter

---
+++ pages/users/_id.vue
@@ -0,0 +1,12 @@
+<template>
+  <h1>{{ $route.params.id}}</h1>
+</template>
+
+<script>
+export default {
+  validate({ params }) {
+    // must be a number
+    return /^\d+$/.test(params.id)
+  }
+}
+</script>
