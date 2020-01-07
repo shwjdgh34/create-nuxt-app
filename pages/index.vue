5d794d06777d68825bb0df2a1eba21d5ce4009b5
Jeongho Noh <nossi8128@gmail.com>
2020-01-07 17:46:22 +0900

    nuxt-link for routing without reload

--- pages/index.vue
+++ pages/index.vue
@@ -1,30 +1,6 @@
 <template>
   <div class="container">
-    <div>
-      <logo />
-      <h1 class="title">
-        nuxt-app
-      </h1>
-      <h2 class="subtitle">
-        create-nuxt-app
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
-    </div>
+    <nuxt-link to="/users">Users</nuxt-link>
   </div>
 </template>
 
